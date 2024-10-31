35e13dee3761c65226e9371a9ecfdfb043d4cc6c
julia <juliareisdasilva2006@gmail.com>
2024-10-31 09:04:38 -0300

    31/10

---
+++ jogoinfinito/App.xaml.cs
@@ -0,0 +1,11 @@
+﻿namespace jogoinfinito;
+
+public partial class App : Application
+{
+	public App()
+	{
+		InitializeComponent();
+
+		MainPage = new AppShell();
+	}
+}
---
+++ jogoinfinito/AppShell.xaml.cs
@@ -0,0 +1,9 @@
+﻿namespace jogoinfinito;
+
+public partial class AppShell : Shell
+{
+	public AppShell()
+	{
+		InitializeComponent();
+	}
+}
